319a78908f8196104329c254783dbc9e76f40e91
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-05-01 07:06:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -96,8 +96,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://vjmasterblastbot-3defdcebeff4.herokuapp.com".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://vjmasterblastbot-3defdcebeff4.herokuapp.com/".format(FQDN, PORT)
+URL = "https://myvjmobiesearch-83769fa3c0a4.herokuapp.com".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://myvjmobiesearch-83769fa3c0a4.herokuapp.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -112,9 +112,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://vjmasterblastbotn-d854b95a53f0.herokuapp.com/".format(FQDN)
+    URL = "https://myvjmobiesearch-83769fa3c0a4.herokuapp.com/".format(FQDN)
 else:
-    URL = "https://vjmasterblastbotn-d854b95a53f0.herokuapp.com/".format(FQDN)
+    URL = "https://myvjmobiesearch-83769fa3c0a4.herokuapp.com/".format(FQDN)
 
 
 
