8b4652ad8d6a9b0b8e263b46d5b080dddf83ccc1
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-10-06 20:31:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '27639102'))
 API_HASH = environ.get('API_HASH', '35142c1407be6264e68fb6bec5dcabd9')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6614613202:AAEi4wsucNweZiYPjll8iVnRbYj7rZzq1zs")
+BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
